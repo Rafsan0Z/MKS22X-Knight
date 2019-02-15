31f3295b7303c5b1254c80feb159f747a5d25fed
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-02-14 20:54:10 -0500

    MarkUp corrected!

--- KnightBoard.java
+++ KnightBoard.java
@@ -28,12 +28,13 @@ public class KnightBoard{
 
   private void MarkUp(int row, int col){
     if(inBounds(row-1,col+3)){Marks[row-1][col+3] += 1;}
-    if(inBounds(row-1,col-3)){Marks[row-1][col-3] += 1;}
-    if(inBounds(row-3,col+1)){Marks[row-3][col+1] += 1;}
-    if(inBounds(row-3,col-1)){Marks[row-3][col-1] += 1;}
-    if(inBounds(row+1,col+3)){Marks[row+1][col+3] += 1;}
-    if(inBounds(row+1,col-3)){Marks[row+1][col-3] += 1;}
-    if(inBounds(row+3,col+1)){Marks[row+3][col+1] += 1;}
+    else if(inBounds(row-1,col-3)){Marks[row-1][col-3] += 1;}
+    else if(inBounds(row-3,col+1)){Marks[row-3][col+1] += 1;}
+    else if(inBounds(row-3,col-1)){Marks[row-3][col-1] += 1;}
+    else if(inBounds(row+1,col+3)){Marks[row+1][col+3] += 1;}
+    else if(inBounds(row+1,col-3)){Marks[row+1][col-3] += 1;}
+    else if(inBounds(row+3,col+1)){Marks[row+3][col+1] += 1;}
+    else if(inBounds(row+3,col-1)){Marks[row+3][col-1] += 1;}
   }
 
   // Make sure that there is a Knight in given position
