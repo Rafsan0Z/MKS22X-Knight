6e3e64542cd9bd39fb9b402daa9962aad54f5c37
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-02-26 10:55:29 -0500

    Small error corrected!

--- KnightBoard.java
+++ KnightBoard.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.*;
 public class KnightBoard{
 
   private int[][] board;
