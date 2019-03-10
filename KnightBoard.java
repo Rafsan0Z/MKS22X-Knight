d75f4af90b408ea9d41b529f0e809ad5b48194d3
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-10 13:38:46 -0400

    Mr. K test passed!

--- KnightBoard.java
+++ KnightBoard.java
@@ -144,13 +144,42 @@ public class KnightBoard{
     return result;
   }
 
+  public static void runTest(int i){
+
+  KnightBoard b;
+  int[]m =   {4,5,5,5,5};
+  int[]n =   {4,5,4,5,5};
+  int[]startx = {0,0,0,1,2};
+  int[]starty = {0,0,0,1,2};
+  int[]answers = {0,304,32,56,64};
+  if(i >= 0 ){
+    try{
+      int correct = answers[i];
+      b = new KnightBoard(m[i%m.length],n[i%m.length]);
+
+      int ans  = b.countSolutions(startx[i],starty[i]);
+
+      if(correct==ans){
+        System.out.println("PASS board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans);
+      }else{
+        System.out.println("FAIL board size: "+m[i%m.length]+"x"+n[i%m.length]+" "+ans+" vs "+correct);
+      }
+    }catch(Exception e){
+      System.out.println("FAIL Exception case: "+i);
+
+    }
+  }
+}
+
   public static void main(String[] args){
 
     KnightBoard kn = new KnightBoard(5,5);
     int row = Integer.parseInt(args[0]);
     int col = Integer.parseInt(args[1]);
+    System.out.println(kn.countSolutions(row,col));
     System.out.println(kn.solve(row,col));
     System.out.println(kn);
+    runTest(3);
 
   }
 
