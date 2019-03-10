dc79ec92ada9b28b3344abaadd634251ace1d45d
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-10 11:46:24 -0400

    exception added to countSolution!

--- KnightBoard.java
+++ KnightBoard.java
@@ -51,6 +51,11 @@ public class KnightBoard{
     if(!inBounds(startingrow,startingcol)){
       throw new IllegalArgumentException();
     }
+    for(int i = 0; i < length; i++){
+      for(int j = 0; j < width; j++){
+        if(board[i][j] != 0){throw new IllegalArgumentException();}
+      }
+    }
     counterHelper(1,startingrow,startingcol);
     return Solutions;
   }
